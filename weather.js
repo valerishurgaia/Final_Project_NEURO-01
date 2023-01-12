c0c12fc4b802aef5b333a48bdad448cb092e2bd6
valerishurgaia <valerishurgo@gmail.com>
2023-01-12 23:43:34 +0400

    weather select added

--- weather.js
+++ weather.js
@@ -5,8 +5,22 @@ const visibilityNumber = document.getElementById('visibilityNumber');
 const FeelsLikeNumber = document.getElementById('FeelsLikeNumber');
 const humidityNumber = document.getElementById ('humidityNumber');
 const windNumber = document.getElementById('windNumber');
+const addButton =document.getElementById('add_button');
+const options = document.getElementById('options');
+const addLocationLlabel = document.getElementById('add_location_label')
+const select_city = document.getElementById('city');
+const description = document.getElementById('description');
 
-const api = "https://api.openweathermap.org/data/2.5/weather?q=Tbilisi&units=metric&appid=3cf41b991f1e7904886436ac9ea90e9a";
+
+
+addButton.addEventListener('click', () => {
+    addButton.classList.toggle('active');
+    addLocationLlabel.classList.toggle('active');
+    options.classList.toggle('visible')
+});
+
+function weatherInlocation () {
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${select_city.value}&units=metric&appid=3cf41b991f1e7904886436ac9ea90e9a`;
 
 fetch(api)
     .then((response) => {
@@ -19,8 +33,16 @@ fetch(api)
         FeelsLikeNumber.innerText = data.main.feels_like;
         humidityNumber.innerText = data.main.humidity;
         windNumber.innerText = data.wind.speed;
+        description.innerText =data.weather[0].description;
     })
+}
+weatherInlocation();
 
+select_city.addEventListener('change', () => {
+    weatherInlocation();
+});
+
+// current day
 const today = new Date();
 
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
